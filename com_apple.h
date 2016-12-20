61aa18d16416e9c03eec2ffb8f8df56b389e6bf9
John Holdsworth <github@johnholdsworth.com>
2016-12-20 13:04:46 +0000

    Initial commit

---
+++ com_apple.h
@@ -0,0 +1,19 @@
+//
+//  com_apple.h
+//  com_apple
+//
+//  Created by John Holdsworth on 28/07/2016.
+//  Copyright © 2016 John Holdsworth. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for com_apple.
+FOUNDATION_EXPORT double com_appleVersionNumber;
+
+//! Project version string for com_apple.
+FOUNDATION_EXPORT const unsigned char com_appleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <com_apple/PublicHeader.h>
+
+
